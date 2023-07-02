e0c78a1dec6520d7bab494560a21f894c1ce4722
Amit Kumar Aich <87881212+NightCoderAmit@users.noreply.github.com>
2023-07-02 19:42:20 +0530

    Javascript Objects

---
+++ Lecture16.js
@@ -0,0 +1,34 @@
+//Objects
+
+let obj = {name: "Cipher"}
+// console.log(obj)
+
+let person = {
+    name: 'Jhon',
+    email: 'jhon@wick.com',
+    age : 45,
+    status: true,
+    children : {
+        name : 'Jay'
+    },
+    hobbies : ['Reading', 'Writing', true, 100, null]
+}
+
+// let clickedName = 'name';
+
+// console.log(person[clickedName]) // !=== person.clickedName
+
+// console.log(typeof person)
+// console.log(person.status);
+// console.log(person['age']); //=== person.age
+// console.log(person['children']);
+
+// console.log(person.clickedName);
+
+// person.country = 'India';
+console.log(person);
+// console.log(year); // cannot acess before inisilitation
+// let year = 2020;
+// person['country'] = 'India';
+// console.log(typeof person.hobbies);
+// console.log(typeof null);
